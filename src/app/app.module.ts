cf3b8f07da0cabddecff16886e9691ba9465f850
cavanosa <gato.cavanosa@gmail.com>
2020-01-16 13:45:01 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaProductoComponent } from './producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// external
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/producto/lista-producto.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Producto } from '../models/producto';
+import { ProductoService } from '../service/producto.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-lista-producto',
+  templateUrl: './lista-producto.component.html',
+  styleUrls: ['./lista-producto.component.css']
+})
+export class ListaProductoComponent implements OnInit {
+
+  productos: Producto[] = [];
+
+  constructor(
+    private productoService: ProductoService,
+    private toastr: ToastrService
+    ) { }
+
+  ngOnInit() {
+    this.cargarProductos();
+  }
+
+  cargarProductos(): void {
+    this.productoService.lista().subscribe(
+      data => {
+        this.productos = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  borrar(id: number) {
+    this.productoService.delete(id).subscribe(
+      data => {
+        this.toastr.success('Producto Eliminado', 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        this.cargarProductos();
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+      }
+    );
+  }
+
+}
